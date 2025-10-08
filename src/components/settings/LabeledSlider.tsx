762277354a392aaf7c286cec1a57f080659060eb
BabyBee34 <sirfpubg12@gmail.com>
2025-10-09 01:57:55 +0300

    fix: Slider import named export olarak değiştirildi - @react-native-community/slider

--- src/components/settings/LabeledSlider.tsx
+++ src/components/settings/LabeledSlider.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
-import Slider from '@react-native-community/slider';
+import { Slider } from '@react-native-community/slider';
 import * as Haptics from 'expo-haptics';
 
 interface LabeledSliderProps {
